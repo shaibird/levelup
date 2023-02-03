45dcb9618a4e3ab40a21444c09c0f91d9b4c2464
Shaina Couch <shainacouch21@gmail.com>
2023-02-03 10:46:35 -0600

    data is kind of messed up. Tried resettign the database, but still had issues loading columns in games. Going with the flow on this one.

--- levelupapi/models/attendees.py
+++ levelupapi/models/attendees.py
@@ -2,4 +2,4 @@ from django.db import models
 
 class Attendees(models.Model):
     event = models.ForeignKey("event", on_delete=models.CASCADE, related_name="event")
-    player = models.ForeignKey("game", on_delete=models.CASCADE, related_name="game")
+    gamer = models.ForeignKey("Gamer", on_delete=models.CASCADE, default=False, related_name="attendee")
--- levelupapi/models/event.py
+++ levelupapi/models/event.py
@@ -4,6 +4,6 @@ class Event(models.Model):
 
     game = models.ForeignKey("Game", on_delete=models.CASCADE, related_name="game_event")
     organizer = models.ForeignKey("Gamer", on_delete=models.CASCADE, related_name="organizer")
-    attendees = models.ManyToManyField("Gamer", related_name="attendees")
+    attendees = models.ManyToManyField("Gamer", through="attendees")
     datetime = models.DateTimeField(auto_now=False, auto_now_add=False)
     description = models.CharField(max_length=200)
